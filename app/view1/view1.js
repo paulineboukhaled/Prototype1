dbfa3d97556a85508824d1133e058cb6c1defce6
paulineboukhaled <pauline.boukhaled@master.hes-so.ch>
2016-01-12 17:47:18 +0100

    Add other fields

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -14,10 +14,25 @@ angular.module('myApp.view1', ['ngRoute'])
 .controller('View1Ctrl', ['$scope', '$http', '$route', function ($scope, $http, $route) {
         $scope.newCandidat = {nom: 'Dupont', prenom: 'Pierre', skill: 'Java_(programming_language)', city: 'Fribourg', npa:'1700', dateofbirth:'2014-02-20', address:'21 avenue du moleson'};
         $scope.checked = true;
+        $scope.prop2 = {
+            "type": "select",
+            "name": "level",
+            "value": "Knowledge",
+            "values": ['Knowledge', 'Experience', 'Deep', 'Expert']
+        };
+
+        $scope.prop = {
+            "type": "select",
+            "name": "yearofexperience",
+            "value": "1",
+            "values": ["1", "2", "3", "4","5","6","7","8","9","10","11", "12", "13", "14","15","16","17","18","19","20","21", "22", "23", "24","25","26","27","28","29","30","31", "32", "33", "34","35","36","37","38","39","40","41", "42", "43", "44","45","46","47","48","49","50"]
+        };
+
 
         $scope.nextStep =  function() {
             $scope.checked = !$scope.checked;
         }
+
 
 
 
